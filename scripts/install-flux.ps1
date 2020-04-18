35e39656edfc20693cb2bcdfacdf879fd125daec
Geert Baeke <geert@baeke.info>
2020-04-18 17:05:16 +0200

    1 replica

--- scripts/install-flux.ps1
+++ scripts/install-flux.ps1
@@ -1,3 +1,13 @@
+# helm operator
+helm repo add fluxcd https://charts.fluxcd.io
+
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.0.0/deploy/crds.yaml
+
+helm upgrade -i helm-operator fluxcd/helm-operator `
+    --set helm.versions=v3
+
+
+# flux installation
 kubectl create ns flux
 
 $GHUSER=$args[0]
